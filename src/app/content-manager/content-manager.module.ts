61ac014a48a30e542b82262e0a20d8919aae644b
Adrian <adrian.herd95@gmail.com>
2021-01-05 02:43:48 -0500

    ContentManager: basic setup

--- src/app/content-manager/content-manager.module.ts
+++ src/app/content-manager/content-manager.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ContentManagerComponent } from './content-manager.component';
 
 @NgModule({
@@ -9,9 +10,10 @@ import { ContentManagerComponent } from './content-manager.component';
   exports: [
     ContentManagerComponent,
   ],
-  imports: [
-    CommonModule,
-  ],
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+    ],
 })
 export class ContentManagerModule {
 }
