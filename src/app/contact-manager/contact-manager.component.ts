4914a856d8aa74ff7aead1edc5b705e0faed1e34
Adrian <adrian.herd95@gmail.com>
2021-01-06 05:59:16 -0500

    Icon updates for better relevance

--- src/app/contact-manager/contact-manager.component.ts
+++ src/app/contact-manager/contact-manager.component.ts
@@ -1,4 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Icon } from '@fortawesome/fontawesome-svg-core';
+import {
+  IconDefinition,
+  faTrash,
+  faPlus,
+  faAngleRight,
+  faAngleDoubleRight,
+  faAngleLeft,
+  faAngleDoubleLeft,
+  faQuoteRight,
+  faTag,
+  faPhoneAlt,
+  faStar,
+  faEnvelope,
+  faCalendarPlus,
+  faReply,
+  faQuestionCircle,
+  faClipboardList,
+  faStickyNote,
+  faSort,
+  faSortUp,
+  faSortDown,
+  faCheckSquare,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faSquare
+} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'content-manager',
@@ -6,6 +33,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-manager.component.scss']
 })
 export class ContactManagerComponent implements OnInit {
+  // add, delete, help
+  addIcon: IconDefinition = faPlus;
+  deleteIcon: IconDefinition = faTrash;
+  helpIcon: IconDefinition = faQuestionCircle;
+  // Table navigation
+  firstPageIcon: IconDefinition = faAngleDoubleLeft;
+  prevPageIcon: IconDefinition = faAngleLeft;
+  nextPageIcon: IconDefinition = faAngleRight;
+  lastPageIcon: IconDefinition = faAngleDoubleRight;
+  // Contact Actions
+  surveyIcon: IconDefinition = faClipboardList;
+  reviewIcon: IconDefinition = faStar;
+  emailIcon: IconDefinition = faEnvelope;
+  appointmentIcon: IconDefinition = faCalendarPlus;
+  replyIcon: IconDefinition = faReply;
+  noteIcon: IconDefinition = faStickyNote;
+  // Lead
+  quoteIcon: IconDefinition = faQuoteRight;
+  callIcon: IconDefinition = faPhoneAlt;
+  estimateIcon: IconDefinition = faTag;
+  // Sort
+  sortNeutral: IconDefinition = faSort;
+  sortAsc: IconDefinition = faSortUp;
+  sortDesc: IconDefinition = faSortDown;
+  // Select
+  uncheckedIcon: IconDefinition = faSquare;
+  checkedIcon: IconDefinition = faCheckSquare;
 
   constructor() { }
 
